4da4e75b0e2bf9c3e133fbc9d90cb24a7ff59f42
guanjun <22811826@qq.com>
2020-09-19 09:23:56 +0800

    V1011

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
       <div class="q-pa-md">
                 <div class="text-h4">
                     金大万翔物联网管理平台
-                    <q-badge align="top">cli v1.0.0</q-badge>
+                    <q-badge align="top">cli v1.1.0</q-badge>
              </div>
        
         <div class="q-gutter-md" style="max-width: 300px">
